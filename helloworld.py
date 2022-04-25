cc907566b7a06127624d524755bee9d13a36a243
DalilaConti <dalila.contigoyeneche@gmail.com>
2022-04-25 08:58:35 -0300

    Exercise first week

---
+++ helloworld.py
@@ -0,0 +1,5 @@
+# Exercise Number one: Programming building blocks cse 110 
+color = input('Please type your favorite color:')
+print('Your favourite color is' + ' ' + color )
+type = input('Do you like the opaque o shinys colors?')
+print(color+ ' ' + type + ' ' + 'it is an awesome choice!')
