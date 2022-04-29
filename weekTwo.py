11242f0d52992da9f60d64770fe43b38b0c30009
DalilaConti <dalila.contigoyeneche@gmail.com>
2022-04-29 08:34:00 -0300

    midLab

---
+++ weekTwo.py
@@ -0,0 +1,19 @@
+presentation = ("Please enter the following:")
+print (presentation)
+adjective = input("adjective:")
+animal = input("animal:")
+verb = input("verb:")
+exclamation = input("exclamation:")
+verb = input ("verb:")
+verb_two = input ("verb:")
+color = input("color:")
+noun = input("noun:")
+
+story = ("Your story is")
+print (story)
+print ("The other" + " " + adjective + " " + "day, I was really in trouble. It all started when I saw a very" + " " + adjective + " " + animal + " " + color +  " " + verb + " " +  "down the hallway." + "'" + exclamation.capitalize() + "!'" + "I yelled. But all I could think to do was to" + " " + verb + " " +  "over and over. Miraculously,that caused it to stop, but not before it tried to" + " " + verb + " " + 
+"right in front of my family" + " " + noun + "." )
+
+
+
+
